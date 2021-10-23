24d7b2bdc2419f7677ec9c7a7035e48693248840
Jonathan Dyallo <emeraldusk@gmail.com>
2021-10-23 17:44:44 -0300

    Initialize project using Create React App

---
+++ scripts/run.js
@@ -0,0 +1,32 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+  
+    let waveCount;
+    let waveByAddress;
+    waveCount = await waveContract.getTotalWaves();
+  
+    let waveTxn = await waveContract.makeWave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+    waveTxn = await waveContract.connect(randomPerson).makeWave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+    waveByAddress = await waveContract.getTotalWavesByAddress(owner.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
